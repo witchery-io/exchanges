54c07794b82935ed827e7fbf8ee69f1a0a85fbe8
Vlad <vlad@witchery.io>
2020-01-06 01:43:30 +0400

    lint fixes

--- pkg/domain/pair.go
+++ pkg/domain/pair.go
@@ -5,18 +5,18 @@ import "strings"
 // CurrencyPair is universal pair type for all exchange clients
 type CurrencyPair string
 
-func (c CurrencyPair) String() string {
-	return string(c)
+func (c *CurrencyPair) String() string {
+	return string(*c)
 }
 
 // GetCur1 get first Currency from pair
-func (c CurrencyPair) GetCur1() Currency {
-	return Currency(strings.Split(string(c), "|")[0])
+func (c *CurrencyPair) GetCur1() Currency {
+	return Currency(strings.Split(string(*c), "|")[0])
 }
 
 // GetCur2 get second Currency from pair
-func (c CurrencyPair) GetCur2() Currency {
-	return Currency(strings.Split(string(c), "|")[1])
+func (c *CurrencyPair) GetCur2() Currency {
+	return Currency(strings.Split(string(*c), "|")[1])
 }
 
 // NewCurrencyPairFrom2Currencies create currency pair from 2 currencies
